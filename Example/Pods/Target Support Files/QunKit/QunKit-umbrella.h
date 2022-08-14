556bb78004738744f42e629b63db5c627249d49f
qun <qunwang6@gmail.com>
2022-08-15 00:39:07 +0800

    0.1.2

--- Example/Pods/Target Support Files/QunKit/QunKit-umbrella.h
+++ Example/Pods/Target Support Files/QunKit/QunKit-umbrella.h
@@ -10,9 +10,36 @@
 #endif
 #endif
 
+#import "NSArray+MD.h"
+#import "NSAttributedString+MD.h"
+#import "NSDate+MD.h"
+#import "NSObject+MD.h"
+#import "NSString+MD.h"
+#import "UIBezierPath+MD.h"
+#import "UIColor+MD.h"
+#import "UIImage+MD.h"
+#import "UIView+DL.h"
+#import "UIView+Frame.h"
+#import "GBProressHudView.h"
+#import "JHUD.h"
+#import "JHUDAnimationView.h"
+#import "UIImage+JHUD.h"
+#import "UIView+JHUD.h"
+#import "DLViewCommonImpl.h"
+#import "MDBaseAlert.h"
+#import "MDButton.h"
+#import "MDFoundation.h"
+#import "MDLabel.h"
+#import "MDView.h"
+#import "MDApp.h"
+#import "MDStringUtils.h"
+#import "MDTimer.h"
+#import "PayManager.h"
+#import "PublishTool.h"
 #import "WebCacheHelpler.h"
 #import "ATHightlightButton.h"
 #import "ATImageTopButton.h"
+#import "UIView+UCCornerRadius.h"
 
 FOUNDATION_EXPORT double QunKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char QunKitVersionString[];
--- QunKit/Classes/MDFoundation/Categories/NSString+MD.h
+++ QunKit/Classes/MDFoundation/Categories/NSString+MD.h
@@ -13,27 +13,12 @@ NS_ASSUME_NONNULL_BEGIN
 ///拼接URL
 - (NSString *)dl_stringByAppendingURLComponent:(NSString *)path;
 
-
 ///计算字符串的大小
 - (CGSize)dl_sizeWithFont:(UIFont *)font maxWidth:(CGFloat)maxWidth;
 
 ///计算字符串的宽度
 - (CGFloat)withString:(UIFont *)font;
 
-//格式化价格超过万的显示
-- (NSAttributedString *)formatWithPrice:(float)bigFont middleFont:(float)middleFont;
-
-- (NSAttributedString *)formatWithPriceDetail:(float)bigFont middleFont:(float)middleFont;
-
-//奇豆超过万的显示
-- (NSString *)formatWithQidou;
-//奇豆超过万的显示，固定两位小数
-- (NSString *)formatWithQidouTwoPoint;
-
-//四舍五入
-- (NSString *)formatWithQidouTwoPointByRounded;
-//去掉小数点
--(NSString*)removeFloatAllZero;
 @end
 
 NS_ASSUME_NONNULL_END
